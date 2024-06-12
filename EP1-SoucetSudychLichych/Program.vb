c414477ed389b32c69f648aef62e2b0adda91197
Eliška Bejčková <eliskabejckova@centrum.cz>
2024-06-12 10:10:59 +0200

    loop

--- EP1-SoucetSudychLichych/Program.vb
+++ EP1-SoucetSudychLichych/Program.vb
@@ -4,14 +4,15 @@ Module Program
   Sub Main(args As String())
         Dim cislo, cislo2 As String
         Dim cisl, cisl2, vysledek As Integer
-
-        Console.WriteLine("Napiš ¨èíslo")
+        Do
+            Console.WriteLine("Napiš èíslo")
             cislo = Console.ReadLine
             Console.WriteLine("Napiš další èíslo")
             cislo2 = Console.ReadLine
             vysledek = cislo + cislo2
-        Console.WriteLine($"Výsledek je {vysledek}")
+            Console.WriteLine($"Výsledek je {vysledek}")
 
+        Loop
 
     End Sub
 End Module
