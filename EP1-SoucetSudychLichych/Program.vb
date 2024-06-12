cf4b2e82868dd13cada5aec6786c0229e18ad7f2
Eliška Bejčková <eliskabejckova@centrum.cz>
2024-06-12 10:14:24 +0200

    parse + jen celá

--- EP1-SoucetSudychLichych/Program.vb
+++ EP1-SoucetSudychLichych/Program.vb
@@ -3,16 +3,24 @@ Imports System
 Module Program
   Sub Main(args As String())
         Dim cislo, cislo2 As String
-        Dim cisl, cisl2, vysledek As Integer
-        Do
-            Console.WriteLine("Napiš èíslo")
+        Dim cisloo, cisloo2, vysled As Integer
+
+        Console.WriteLine("Napiš èíslo")
             cislo = Console.ReadLine
             Console.WriteLine("Napiš další èíslo")
             cislo2 = Console.ReadLine
-            vysledek = cislo + cislo2
-            Console.WriteLine($"Výsledek je {vysledek}")
+            vysled = cislo + cislo2
+            Console.WriteLine($"Výsledek je {vysled}")
+            If Integer.TryParse(cislo, cisloo) And
+                    Integer.TryParse(cislo2, cisloo2) Then
+                If cisloo >= 0 And cisloo2 >= 0 Then
+                    vysled = cisloo + cisloo2
+                    Console.WriteLine($"Výsledek je: {vysled}")
+                Else
+                    Console.WriteLine(" pouze celá èísla :)")
+                End If
+            End If
 
-        Loop
 
     End Sub
 End Module
