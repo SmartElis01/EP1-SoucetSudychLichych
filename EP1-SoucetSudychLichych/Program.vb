ad6f95ee5133f1fdd25f8406fd64288493905535
Eliška Bejčková <eliskabejckova@centrum.cz>
2024-06-12 10:09:49 +0200

    začátek

--- EP1-SoucetSudychLichych/Program.vb
+++ EP1-SoucetSudychLichych/Program.vb
@@ -2,6 +2,16 @@ Imports System
 
 Module Program
   Sub Main(args As String())
-    Console.WriteLine("Hello World!")
-  End Sub
+        Dim cislo, cislo2 As String
+        Dim cisl, cisl2, vysledek As Integer
+
+        Console.WriteLine("Napiš ¨èíslo")
+            cislo = Console.ReadLine
+            Console.WriteLine("Napiš další èíslo")
+            cislo2 = Console.ReadLine
+            vysledek = cislo + cislo2
+        Console.WriteLine($"Výsledek je {vysledek}")
+
+
+    End Sub
 End Module
